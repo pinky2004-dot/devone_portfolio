0c37cdc6737f9c0c11a1b57d51926fc97add05ae
Sai Donepudi <saidonepudi2004@gmail.com>
2025-09-16 20:51:40 -0500

    feat: Optimize 3D model for performance

--- src/components/models/hero_models/BookCart.jsx
+++ src/components/models/hero_models/BookCart.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { useGLTF } from "@react-three/drei";
 
 export function BookCart(props) {
-  const { scene } = useGLTF("/models/cart.glb");
+  const { scene } = useGLTF("/models/optimized_cart.glb");
   return <primitive object={scene} {...props} />;
 }
 
-useGLTF.preload("/models/cart.glb");
+useGLTF.preload("/models/optimized_cart.glb");
